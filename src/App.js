b20bf700c09ddf04b09d3bdd5bae31a23c1f9a7d
Aziz Ali <aziz@ilovecoding.org>
2019-12-11 14:43:23 -0600

    Styles

--- src/App.js
+++ src/App.js
@@ -1,47 +1,23 @@
+import './style.css'
 import React from 'react';
-import Wrapper from './Wrapper'
-import Greeting from './Greeting'
-// Just a function that return a user interface
-// Component name should be Capitalized
-// User interface HTML
-// Import React library
-// Always ONE parent element
+
 function App(){
-  // If you want to render based on multiple conditions
-  function shouldRender (isLoading, hasData){
-    return true || false
-  }
-  const isLoading = true
-  const name = "Intuit Team"
   return (
-    <> 
-      {shouldRender() && <div>Loading...</div>}
-      <div>Hello world, some more text, Test again</div>
-      <hr />
-      {!shouldRender() && <Wrapper />}
-      <br/>
-      {!shouldRender() && <Wrapper>
-        information, data
-      </Wrapper>}
-      <hr />
-      <Greeting
-        personName={name}
-        personAge={12}
-        doSomething={()=>{
-          // alert('hello')
-          return <div>Did something</div>
-        }}
-        locations={['Chicago', 'San Diego', 'NYC']}
-        options={{
-          employeeCount: 1000,
-          numberOfCustomers: Infinity
-        }}
-      />
+    <>
+      <div style={{
+        backgroundColor: 'red',
+        color: 'white'
+      }}>
+        This is a React box 1
+      </div>
+      <div className="boxy">
+        This is a React box 2
+      </div>
+      <div className="boxy2">
+        This is a React box 3
+      </div>
     </>
   )
 }
-// Default export
-// export default App
 
-// // Named export
 export { App }
